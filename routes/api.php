49fac51b31890e5af4083f3f54977072675a787d
Lucas Gabriel <lucas.ddd@hotmail.com>
2022-08-22 21:55:05 -0300

    Módulo de Lessons criado

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\Api\ModuleController;
+use App\Http\Controllers\Api\LessonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,9 @@ Route::get('/courses', [CourseController::class, 'index']);
 Route::get('/courses/{id}', [CourseController::class, 'show']);
 
 Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+
+Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+Route::get('/lessons/{id}', [LessonController::class, 'show']);
 
 Route::get('/', function () {
     return response()->json([
